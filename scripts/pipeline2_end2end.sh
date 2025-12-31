7326569dbaa806dfb894526b1ecaf0408d03884e
tbao04 <tbao041024@gmail.com>
2025-12-31 17:38:13 +0700

    add flag model & prune FPN or not

--- scripts/pipeline2_end2end.sh
+++ scripts/pipeline2_end2end.sh
@@ -6,6 +6,7 @@ DATA_ROOT="./NewDeepfish/NewDeepfish"
 OUTPUT_ROOT="./output/pipeline2"
 BATCH_SIZE=8
 WORKERS=16
+MODEL="fasterrcnn_resnet18_fpn"
 
 echo "======================================================="
 echo "STARTING PIPELINE 2: END-TO-END DETECTION PRUNING"
@@ -19,6 +20,7 @@ echo "======================================================="
 echo "[Step 1/3] Training Dense Faster R-CNN..."
 python train_det.py \
     --data-path $DATA_ROOT \
+    --model $MODEL \
     --epochs 60 \
     --batch-size $BATCH_SIZE \
     --workers $WORKERS \
@@ -34,6 +36,7 @@ echo "[Step 2/3] Iterative Pruning (SongHan + Filter)..."
 # Lưu ý: Script này sẽ tự động gọi Surgery ở cuối để tạo model_lean.pth
 python prune_det.py \
     --data-path $DATA_ROOT \
+    --model $MODEL \
     --checkpoint $OUTPUT_ROOT/step1_dense_det/model_best.pth \
     --target-sparsity 0.5 \
     --prune-iters 8 \
@@ -48,6 +51,7 @@ python prune_det.py \
 echo "[Step 3/3] Final Finetuning..."
 python train_det.py \
     --data-path $DATA_ROOT \
+    --model $MODEL \
     --weights-backbone $OUTPUT_ROOT/step2_pruned_det/model_lean.pth \
     --compress-rate $OUTPUT_ROOT/step2_pruned_det/model_lean.json \
     --epochs 50 \
